4e4ce5ebdbcd435a47722906f23bfb5b4879dd24
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-06 12:36:57 -0800

    to do list component setup

---
+++ src/assets/data/index.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        title: "Get a job",
+        complete: false
+    },{
+        title: "Eat",
+        complete: false
+    },{
+        title: "Do yo job",
+        complete: false
+    },{
+        title: "Wake Up",
+        complete: true
+    }
+];
