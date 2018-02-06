744fc2f40f8d0ef0cf6752aad8cf5ff2f8d1f9df
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-06 14:28:24 -0800

    added functionality to add new items to the list

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,15 +1,11 @@
 import React, {Component} from "react";
-import toDoData from "../assets/data";
 class ToDoList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        this.state = {
-            list: toDoData
-        }
     }
+
     render(){
-        const listItems = this.state.list.map((item, index) => {
+        const listItems = this.props.list.map((item, index) => {
             return <li key={index} className="collection-item">{item.title}</li>
         });
         return(
